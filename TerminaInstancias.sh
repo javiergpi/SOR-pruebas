a36d6ad79926f01e7e59accfd31d76d0e5eb403f
Javi G Pisano <javiergpi@educastur.org>
2022-10-29 06:26:43 +0200

    DeleteVPC

--- TerminaInstancias.sh
+++ TerminaInstancias.sh
@@ -18,11 +18,36 @@
 
 
 
-#  vpc=$(aws ec2 --region ${region} \
-#     describe-vpcs --filter Name=isDefault,Values=true \
-#     | jq -r .Vpcs[0].VpcId)
-#   if [ "${vpc}" = "null" ]; then
-#     echo "No default vpc found"
-#     continue
-#   fi
-#   echo "Found default vpc ${vpc}"
+ # get default vpc
+  vpc=$( aws ec2  describe-vpcs --filter Name=isDefault,Values=true --output text --query 'Vpcs[0].VpcId' )
+  if [ "${vpc}" = "None" ]; then
+    echo "${INDENT}No default vpc found"
+    continue
+  fi
+  echo "${INDENT}Found default vpc ${vpc}"
+
+  # get internet gateway
+  igw=$(aws ec2 describe-internet-gateways --filter Name=attachment.vpc-id,Values=${vpc}  --output text --query 'InternetGateways[0].InternetGatewayId' )
+  if [ "${igw}" != "None" ]; then
+    echo "${INDENT}Detaching and deleting internet gateway ${igw}"
+    aws ec2 detach-internet-gateway --internet-gateway-id ${igw} --vpc-id ${vpc}
+    aws ec2 delete-internet-gateway --internet-gateway-id ${igw}
+  fi
+
+  # get subnets
+  subnets=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=${vpc} --output text --query 'Subnets[].SubnetId' )
+  if [ "${subnets}" != "None" ]; then
+    for subnet in ${subnets}; do
+      echo "${INDENT}Deleting subnet ${subnet}"
+      aws ec2 delete-subnet --subnet-id ${subnet}
+    done
+  fi
+
+  # https://docs.aws.amazon.com/cli/latest/reference/ec2/delete-vpc.html
+  # - You can't delete the main route table
+  # - You can't delete the default network acl
+  # - You can't delete the default security group
+
+  # delete default vpc
+  echo "${INDENT}Deleting vpc ${vpc}"
+  aws ec2 delete-vpc --vpc-id ${vpc}
