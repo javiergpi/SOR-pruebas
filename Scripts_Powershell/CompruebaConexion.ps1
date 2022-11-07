6198d6ac001ecadf03521f9fcbe64e9ec198bb1b
Javi G Pisano <javiergpi@educastur.org>
2022-11-07 07:29:25 +0100

    Refinitiva total

--- Scripts_Powershell/CompruebaConexion.ps1
+++ Scripts_Powershell/CompruebaConexion.ps1
@@ -1,7 +1,7 @@
 $ping=$false;
 While(!$ping){
     echo "Probando conectividad..."
-    $ping=Test-Connection SERVIDOR-69.dominio69.local -Quiet
+    $ping=Test-Connection SERVIDOR-66.dominio66.local -Quiet
     Start-Sleep -Seconds 5
     if($ping){
         echo "Hay conectividad"
